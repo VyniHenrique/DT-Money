244da3ac4cd0ce9d5b8df68b372e718ff40ceadf
Vynicius <vynicius.henrique15@gmail.com>
2025-04-04 14:35:30 -0300

    commit 2

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,10 +1,41 @@
 import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
+import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions () {
 
 	return(
 		<div>
 			<Header/>
+			<Summary/>
+
+			<TransactionsContainer>
+				<TransactionsTable>
+					<tbody>
+						<tr>
+							<td width="50%"> Desenvolvimento de site</td>
+							<td>
+								<PriceHighLight variant="income">
+									R$ 12.000,00
+								</PriceHighLight>
+							</td>
+							<td>Venda</td>
+							<td>13/04/2022</td>
+						</tr>
+
+						<tr>
+							<td width="50%"> Hambúrger</td>
+							<td>
+								<PriceHighLight variant="outcome">
+									R$ -60,00
+								</PriceHighLight>
+							</td>
+							<td>Alimentos</td>
+							<td>10/04/2022</td>
+						</tr>
+					</tbody>
+				</TransactionsTable>
+			</TransactionsContainer>
 		</div>
 	)
 }
