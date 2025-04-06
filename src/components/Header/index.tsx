6fc6b3551b445c71aba3a5bef7dec3b166068fda
Vynicius <vynicius.henrique15@gmail.com>
2025-04-05 22:44:05 -0300

    adicionando o SearchForm

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,29 @@
 import { HeaderContent, HearderContainer, NewTransactionButton } from "./styles";
 
+
 import logoImg from "../../assets/logo.svg"
+import * as Dialog from "@radix-ui/react-dialog";
 export function Header () {
 
 	return(
 		<HearderContainer>
 			<HeaderContent>
 				<img src={logoImg} />
-				<NewTransactionButton> Nova transação</NewTransactionButton>
+				
+				<Dialog.Root>
+					<Dialog.Trigger asChild>
+						<NewTransactionButton> Nova transação</NewTransactionButton>
+					</Dialog.Trigger>
+
+					<Dialog.Portal>
+						<Dialog.Overlay>
+							<Dialog.Content>
+								<Dialog.Title>Nova Transação</Dialog.Title>
+								<Dialog.Close />
+							</Dialog.Content>
+						</Dialog.Overlay>
+					</Dialog.Portal>
+				</Dialog.Root>
 			</HeaderContent>
 		</HearderContainer>
 	)
