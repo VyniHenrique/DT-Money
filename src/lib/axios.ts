e3f151f2eda65e409c7efed635a5a2dd31127384
Vynicius Henrique <141872775+VyniHenrique@users.noreply.github.com>
2025-04-23 17:36:49 -0300

    Update axios.ts

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-	baseURL: 'https://json-server-dt-money-gray.vercel.app/transactions'
+	baseURL: 'https://json-server-dt-money-gray.vercel.app'
 	
 });
